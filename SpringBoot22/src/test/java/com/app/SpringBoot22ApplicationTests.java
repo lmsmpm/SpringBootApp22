7f413f897412e97ed7130fc6a5e4763199c48084
manu12 <manu@gmail.com>
2020-06-10 12:18:12 +0530

    Project structure Commit

---
+++ SpringBoot22/src/test/java/com/app/SpringBoot22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
